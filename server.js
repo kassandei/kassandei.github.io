9af923e7aa75004ae2c60c41100939214aa1a001
Kassandei <skyfox839@gmail.com>
2024-11-27 12:36:03 +0100

    new

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Serve static files from the "public" folder
 app.use(express.static('public'));
 
+// Define the root route to serve the HTML file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+// Handle WebSocket connections
 io.on('connection', (socket) => {
   console.log('A user connected');
   
-  // Listen for messages and broadcast to other users
+  // Listen for chat messages and broadcast to all clients
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
